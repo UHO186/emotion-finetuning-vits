d1ef7b1ebc652f8aff855ba82a786f20de4189f7
UHO <82763677+UHO186@users.noreply.github.com>
2023-05-25 11:16:23 +0900

    Update models.py
    AttributeError: 'NoneType' object has no attribute 'unsqueeze'

--- models.py
+++ models.py
@@ -153,8 +153,6 @@ class TextEncoder(nn.Module):
     self.p_dropout = p_dropout
 
     self.emb = nn.Embedding(n_vocab, hidden_channels)
-    self.emo_proj = nn.Linear(1024, hidden_channels)
-
     nn.init.normal_(self.emb.weight, 0.0, hidden_channels**-0.5)
 
     self.encoder = attentions.Encoder(
@@ -166,9 +164,8 @@ class TextEncoder(nn.Module):
       p_dropout)
     self.proj= nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
-  def forward(self, x, x_lengths, emo):
+  def forward(self, x, x_lengths):
     x = self.emb(x) * math.sqrt(self.hidden_channels) # [b, t, h]
-    x = x + self.emo_proj(emo.unsqueeze(1))
     x = torch.transpose(x, 1, -1) # [b, h, t]
     x_mask = torch.unsqueeze(commons.sequence_mask(x_lengths, x.size(2)), 1).to(x.dtype)
 
